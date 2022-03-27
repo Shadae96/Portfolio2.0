73826e5545d62df7959092e8120b7247848d8279
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2022-03-26 21:57:10 -0400

    fixed resume file name

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 // import Particle from "../Particle";
-import pdf from "/Users/shadae/bootcamp/homework/Portfolio2.0/react-portfolio/src/Assets/Projects/Shadae BrownResume.pdf"
+import pdf from "/Users/shadae/bootcamp/homework/Portfolio2.0/src/Assets/Projects/ShadaeBrownResume.pdf"
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
